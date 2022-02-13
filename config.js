6982967aabd9d350a0aa922d10c141fa126d3221
Dongngo24102002 <99591511+Dongngo24102002@users.noreply.github.com>
2022-02-13 20:29:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Gửi Crush",
-    introTitle: 'Crush yêu dấu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Đó có phải là yêu hay không Tại sao làm tym anh ngây dại :>>',
+    titleWeb: "Alo Alo",
+    introTitle: 'Gọi cậu đấy',
+    introDesc: `Ahihi đồ ngốcccc`,
+    btnIntro: '^^Bấm đây này^^',
+    title: 'Đó có phải là yêu hay không, tại sao làm tym mình ngây dại :>>',
     desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu Nam nhất luôn <333',
+    reply: 'Yêu Đông nhất luôn <333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/quyvuongnamsama/' //link mess của các bạn. VD: /dinhkaito
+    messLink: 'https://m.me/anhdongdeptrai2002/' //link mess của các bạn. VD: /dinhkaito
 }
