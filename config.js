6db1c08cd2c015fded5a6258751bc7349f1fdcd8
Dongngo24102002 <99591511+Dongngo24102002@users.noreply.github.com>
2022-02-13 22:28:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "Ngô Ngọc Đông gửi cho bạn",
+    titleWeb: "Nguyet Hoa gửi cho bạn",
     introTitle: ' Alo alo . Gọi bạn đấy',
     introDesc: `Ahihi đồ ngốcccc`,
-    btnIntro: '^^Bấm đây đi ❤️',
-    title: 'Ngô Ngọc Đông có đẹp trai khôngggggg ? Ỏooooooo ^^ ',
+    btnIntro: 'Bấm đây đi ❤️',
+    title: 'Hoa có cuteee khôngggggg ? Ỏooooooo ^^ ',
     desc: 'Thoát web là ăn đập nhé!!!',
-    btnYes: 'Đẹp trai nhất',
+    btnYes: 'Cuteee nhất í',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại khen mình đẹp trai nhất <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại khen mình cuteee nhất <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Vì thích á',
     mess: 'Mình biết mà. Ngại quá ngại quá ❤️❤️❤️❤️',
     messDesc: 'Đi chơi với mình nhá ?',
     btnAccept: 'Vâng ',
-    messLink: 'https://m.me/anhdongdeptrai2002/' 
+    messLink: 'https://m.me/nguyethoa.202/' 
 }
