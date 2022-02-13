fdc4c3ffba2da0da837fa735e811b39f86081148
Dongngo24102002 <99591511+Dongngo24102002@users.noreply.github.com>
2022-02-13 20:39:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu Đông nhất luôn <333',
+    reply: 'Vì Đông đẹp traiiii á',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Đi chơi với mình nhá ?.',
+    btnAccept: 'Vâng ',
     messLink: 'https://m.me/anhdongdeptrai2002/' //link mess của các bạn. VD: /dinhkaito
 }
