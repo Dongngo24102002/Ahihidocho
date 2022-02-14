5e0803aa612ea2f9ed608eaf725c646cc0dad7c4
Dongngo24102002 <99591511+Dongngo24102002@users.noreply.github.com>
2022-02-14 16:51:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    titleWeb: "Nguyet Hoa gửi cho bạn",
-    introTitle: ' Alo alo . Gọi bạn đấy',
-    introDesc: `Ahihi đồ ngốcccc`,
-    btnIntro: 'Bấm đây đi ❤️',
-    title: 'Hoa có cuteee khôngggggg ? Ỏooooooo ^^ ',
-    desc: 'Thoát web là ăn đập nhé!!!',
-    btnYes: 'Cuteee nhất í',
+    titleWeb: "Gửi crush của tớ",
+    introTitle: 'Crush yêu dấu',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
+    btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại khen mình cuteee nhất <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Vì thích á',
-    mess: 'Mình biết mà. Ngại quá ngại quá ❤️❤️❤️❤️',
-    messDesc: 'Đi chơi với mình nhá ?',
-    btnAccept: 'Vâng ',
-    messLink: 'https://m.me/nguyethoa.202/' 
-}
+    reply: 'Yêu Đoàn',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/doanne2003/'
